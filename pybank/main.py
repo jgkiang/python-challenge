57667c4781ec3f75aa83b86252a8a6924872c688
jgkiang <95582256+jgkiang@users.noreply.github.com>
2022-01-25 15:23:48 -0800

    Update main.py

--- pybank/main.py
+++ pybank/main.py
@@ -1,3 +1,10 @@
 import pandas as pd
 
-months = pd.read_csv(
+months = pd.read_csv('budget_data.csv')
+df = pd.read_csv('budget_data.csv')
+
+print ("Total Months: ", len(months)-1)
+print ("Total: {}" .format(df[''].sum())) 
+
+
+
