c44f598099cb005394179f3caeb5b9e4b2224ab8
jgkiang <95582256+jgkiang@users.noreply.github.com>
2022-01-25 15:59:54 -0800

    Update main.py

--- pybank/main.py
+++ pybank/main.py
@@ -5,9 +5,8 @@ for row in open("budget_data.csv"):
     num_rows += 1
 print("Total Months: " +num_rows)
 
-
 with open('budget_data.csv') as csvfile:
   csvfile.next()
   total = sum(int(r[1]) for r in csv.reader(csvfile))
-print(total)
+print("Total: " +total)
 
